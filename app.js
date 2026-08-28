771986f933b50eff3c820e84ccde0ce26152d0c9
digia-dev <hi.digia@gmail.com>
2026-08-28 18:42:19 +0700

    Add .cpanel.yml for auto npm install; simplify app.js

--- app.js
+++ app.js
@@ -1,12 +1,1 @@
-const { execSync } = require('child_process');
-const fs = require('fs');
-const path = require('path');
-
-const nodeModules = path.join(__dirname, 'node_modules');
-if (!fs.existsSync(nodeModules) || !fs.existsSync(path.join(nodeModules, 'express'))) {
-  console.log('Installing dependencies...');
-  const npm = path.join('/home/giantar1/nodevenv/ai/22/bin/npm');
-  execSync(`"${npm}" install --production`, { cwd: __dirname, stdio: 'inherit' });
-}
-
 require('./server.js');
